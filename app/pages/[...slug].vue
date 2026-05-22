9048f6110abc84a18f3739c6175f6416f31b3f9f
thais <thaisgo.viana@gmail.com>
2026-05-21 23:03:54 -0300

    fix: index routo is now open

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -5,7 +5,7 @@ const route = useRoute()
 const { prev, next } = useNavigation(route.path)
 
 const { data: page } = await useAsyncData('page-' + route.path, () => {
-  return queryCollection('content').path(route.path).first()
+  return queryCollection('content').path(route.path === '/' ? '/index' : route.path).first()
 })
 
 if (!page.value) {
