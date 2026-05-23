94c41f3f0b9b7d2d8e4edd4b2f36395600f22ada
thais <thaisgo.viana@gmail.com>
2026-05-22 23:05:51 -0300

    layout: new simple layout for home route

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,36 @@
 <template>
-  <section class="flex flex-col gap-8 max-w-4xl mx-auto px-8 py-5">
+  <section class="flex flex-col gap-5 max-w-4xl mx-auto px-8 py-5">
     <h1 class="text-5xl font-bold text-[#a7f3d0]">
       Myth of Yggdrasil Wiki
     </h1>
 
-    <p>Compilando informações soltas do <a href="https://discord.com/invite/mythofyggdrasil">discord</a>  já que não tem uma wiki oficial!</p>
+    <p>Compilando informações soltas do 
+      <a 
+        href="https://discord.com/invite/mythofyggdrasil" 
+        class="text-emerald-400 hover:text-emerald-300 underline underline-offset-4 transition-colors">discord</a>  
+      já que não tem uma wiki oficial!</p>
 
-    <h3>Créditos</h3>
-    <p>Tudo aqui é de propriedade da galera que postou no discord então vamos lá: </p>
+    <h2 class="text-3xl font-bold text-emerald-300 mb-2 flex items-center gap-3">
+      <span class="text-emerald-500">✦</span>
+      Créditos
+    </h2>
 
-    <ul>
-      <li>Guia do Jogo é o guia completo do usuário OzziCombo (nick no discord segundo o documento dele)</li>
-      <li>Guia do campo de treinamento do usuário Life (nick no discord segundo a postagem dele)</li>
+    <p class="text-zinc-400 leading-8">Tudo aqui é de propriedade da galera que postou no discord então vamos lá: </p>
+
+    <ul class="space-y-4 pl-6 list-disc marker:text-emerald-400 text-zinc-300">
+      <li>
+        <strong class="text-emerald-300">Guia do Jogo</strong>
+            — guia completo criado pelo usuário
+            <mark>OzziCombo</mark>.
+      </li>
+
+      <li>
+        <strong class="text-emerald-300">
+              Guia do Campo de Treinamento
+            </strong>
+            — criado pelo usuário
+            <mark>Life</mark>.
+      </li>
     </ul>
   </section>
 </template>
