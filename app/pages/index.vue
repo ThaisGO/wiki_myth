237d83e200a0d7a8b0eed90e1db2c407f18e168f
thais <thaisgo.viana@gmail.com>
2026-05-25 00:23:53 -0300

    fix: syntax fixing

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,36 +1,42 @@
 <template>
   <section class="flex flex-col gap-5 max-w-4xl mx-auto px-8 py-5">
-    <h1 class="text-5xl font-bold text-[#a7f3d0]">
-      Myth of Yggdrasil Wiki
-    </h1>
+     <div class="flex gap-4 items-center">
+      <img src="/logo.png" alt="Logo" class="w-12 h-12 rounded-lg" />
 
-    <p>Compilando informações soltas do 
-      <a 
-        href="https://discord.com/invite/mythofyggdrasil" 
-        class="text-emerald-400 hover:text-emerald-300 underline underline-offset-4 transition-colors">discord</a>  
-      já que não tem uma wiki oficial!</p>
+      <h1 class="text-5xl font-bold text-[#a7f3d0]">
+        Myth of Yggdrasil Wiki
+      </h1>
+     </div>
 
-    <h2 class="text-3xl font-bold text-emerald-300 mb-2 flex items-center gap-3">
-      <span class="text-emerald-500">✦</span>
-      Créditos
-    </h2>
+      <p>Compilando informações soltas do 
+        <a 
+          href="https://discord.com/invite/mythofyggdrasil" 
+          class="text-emerald-400 hover:text-emerald-300 underline underline-offset-4 transition-colors">discord
+        </a>  
+        já que não tem uma wiki oficial!
+      </p>
 
-    <p class="text-zinc-400 leading-8">Tudo aqui é de propriedade da galera que postou no discord então vamos lá: </p>
+      <h2 class="text-3xl font-bold text-emerald-300 mb-2 flex items-center gap-3">
+        <span class="text-emerald-500">✦</span>
+        Créditos
+      </h2>
 
-    <ul class="space-y-4 pl-6 list-disc marker:text-emerald-400 text-zinc-300">
-      <li>
-        <strong class="text-emerald-300">Guia do Jogo</strong>
-            — guia completo criado pelo usuário
-            <mark>OzziCombo</mark>.
-      </li>
+      <p class="text-zinc-400 leading-8">Tudo aqui é de propriedade da galera que postou no discord então vamos lá: </p>
 
-      <li>
-        <strong class="text-emerald-300">
-              Guia do Campo de Treinamento
-            </strong>
-            — criado pelo usuário
-            <mark>Life</mark>.
-      </li>
-    </ul>
+      <ul class="space-y-4 pl-6 list-disc marker:text-emerald-400 text-zinc-300">
+        <li>
+          <strong class="text-emerald-300">Guia do Jogo</strong>
+              — guia completo criado pelo usuário
+              <mark>OzziCombo</mark>.
+        </li>
+
+        <li>
+          <strong class="text-emerald-300">
+                Guia do Campo de Treinamento
+              </strong>
+              — criado pelo usuário
+              <mark>Life</mark>.
+        </li>
+      </ul>
   </section>
 </template>
